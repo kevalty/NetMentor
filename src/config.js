fdcfd57a143e81ec33f01a2203ce8b335c273cd2
kevalty <kevin.bedon@espoch.edu.ec>
2024-06-25 22:04:19 -0500

    test

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://netmentor.tech/api/";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://netmentor.tech/api/";
 
 export default API_BASE_URL;
